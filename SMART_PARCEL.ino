a7954a093e52949499518da4862d3571a94338da
Sittie Rheham Farouq <93106601+sttrhhm@users.noreply.github.com>
2022-01-10 19:56:38 +0800

    Update SMART_PARCEL.ino

--- SMART_PARCEL.ino
+++ SMART_PARCEL.ino
@@ -40,6 +40,17 @@ Serial.print(a[10]);
 Serial.print(a[11]);
 Serial.println(a[12]);
 
+if (a[0] == '8' && a[1] == '8' && a[2] == '5' && a[3] == '1' && a[4] == '9' && a[5] == '5' && a[6] == '9' && a[7] == '1' && a[8] == '3' && a[9] == '2' && a[10] == '1' && a[11] == '6' && a[12] == '6') {
+Serial.print("Name : ");
+Serial.println("Fanta Orange");
+Serial.println();
+}
+if (a[0] == '8' && a[1] == '8' && a[2] == '5' && a[3] == '1' && a[4] == '9' && a[5] == '5' && a[6] == '9' && a[7] == '1' && a[8] == '3' && a[9] == '2' && a[10] == '1' && a[11] == '7' && a[12] == '3') {
+Serial.print("Name : ");
+Serial.println("Fanta Strawberry");
+Serial.println();
+}
+k = 0;
 }
 }
 }
